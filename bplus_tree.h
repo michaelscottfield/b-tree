c6f60d1697584557b8efb988ee68184d937e2168
michaelscottfield <jr1111@mail.ustc.edu.cn>
2017-08-23 14:56:08 +0800

    minor advancement

--- bplus_tree.h
+++ bplus_tree.h
@@ -2,10 +2,16 @@
 #define BPLUS_NODE
 
 #define NULL 0
+
+#include<vector>
+extern "C"
+
 namespace bptree{
   NodeType root*;
   int m;//子节点的最大数目
   int nodeCount;
+  typedef vector<NodeType*> vec;
+  typedef long int li;
 
   class bplusTree{  
    tydef struct { 
@@ -14,23 +20,22 @@ namespace bptree{
      boolean isLeaf;
      int keyNum;//关键字数目
      int childNum;//子节点数目
-     ItemType Key[4];
-     long Branch[5];
      NodeType* parent;
-     NodeType* children[4];
+     vec children;
+     li  keys;
       
    }NodeType;
    
-   void buildTree(){
-   
-   }
    
    void deleteNode(){
    
    }
    
-   void insertNode(){
-     if (nodeCount == 0){
+   void insertNode(li key, bplusTree tree){
+     if (tree -> nodeCount == 0){
+         NodeType *tempNode;
+	 tempNode -> isRoot = true;
+
      }
    }
 
